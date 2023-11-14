3989fb0c546694e390a672363c63f6d54f083238
R-monisha2000 <141928876+R-monisha2000@users.noreply.github.com>
2023-11-14 21:17:40 +0530

    Update Signup.js

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -202,9 +202,9 @@ function Signup() {
                   Sign up
                     </button>
                      <div className="extra-buttons">
-                  <p className="or">&#x2015; Or &#x2015;</p>
+                  <p className="or">&#x2015; &#x2015;</p>
                   <button onClick={GoogleAuth} className="google">
-                    <p>Sign in with</p>
+                    <
                     <img src={google} className="google-img" />
                   </button>
                 </div>
