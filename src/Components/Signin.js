5834fc8fabacd3cce295a2b0df611a4a97d14788
R-monisha2000 <141928876+R-monisha2000@users.noreply.github.com>
2023-11-14 21:26:31 +0530

    Update Signin.js

--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -147,7 +147,6 @@ function Signin() {
                 <div className="extra-buttons">
                   <p className="or">&#x2015; &#x2015;</p>
                   <button onClick={GoogleAuth} className="google">
-                    
                     <img src={google} className="google-img" />
                   </button>
                 </div>
